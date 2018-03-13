278123bd7719c7b539c51f64c15be7c7c16fde66
Ali Mujthaba <a.mujthaba321@gmail.com>
2018-03-13 19:27:59 +0500

    implemented redux promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,18 @@
+import axios from 'axios';
+
 const WEATHER_API_KEY= '485349437deba1a5b0840d2b3836007b';
 
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${WEATHER_API_KEY}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${WEATHER_API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(cityName){
     const finalURL = `${ROOT_URL}&q=${cityName},us`;
+    const weatherRequest = axios.get(finalURL); 
+
+    console.log("request", weatherRequest);
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: weatherRequest
     };
 }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index'
 
-export default class SearchBar extends Component{
+class SearchBar extends Component{
 
     constructor(props){
         super(props);
@@ -11,7 +14,6 @@ export default class SearchBar extends Component{
     }
 
     onInputChange(e){
-        console.log(e.target.value);
         this.setState({
             searchTerm : e.target.value
         });
@@ -19,6 +21,11 @@ export default class SearchBar extends Component{
     
     onFormSubmit(e) {
         e.preventDefault();
+        console.log("some");
+        this.props.fetchWeather(this.state.searchTerm);
+        this.setState({
+            searchTerm:''
+        });
 
     }
 
@@ -34,7 +41,7 @@ export default class SearchBar extends Component{
 
                         <span className="input-group-btn">
                             <button className="btn btn-default" name="search"
-                            type="button" >Go!</button>
+                            type="submit " >Go!</button>
                         </span>
                 </form>
             </div>
@@ -42,3 +49,9 @@ export default class SearchBar extends Component{
     }
 
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
